5c184af44f1855d5801e69a90598c526514a908f
jayeshtl <jayesh@thingslinker.com>
2026-06-12 21:37:27 +0530

    feat : generate modules users and bookmarks

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { BookmarksModule } from './bookmarks/bookmarks.module';
 
 // Decorator
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UsersModule, BookmarksModule],
   controllers: [],
   providers: [],
 })
