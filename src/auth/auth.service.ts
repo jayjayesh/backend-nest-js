0efb19bda355776084157ef03e6c70b564920a6f
jayeshtl <jayesh@thingslinker.com>
2026-06-16 14:27:42 +0530

    feat : register new user in db and login api implemented

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
@@ -6,16 +6,37 @@ import * as argon from 'argon2';
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signIn(body: AuthDto) {
-    return {
-      email: body.email,
-      password: body.password,
-    };
+  async signIn(body: AuthDto) {
+    ///
+    const userObjc = await this.prisma.user.findUniqueOrThrow({
+      where: { email: body.email },
+    });
+    if (!userObjc) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+
+    ///
+    const pwMatch = await argon.verify(userObjc.password, body.password);
+    if (!pwMatch) {
+      throw new ForbiddenException('Credentials incorrect Password');
+    }
+
+    ///
+    const safeUser = await this.prisma.user.findUniqueOrThrow({
+      where: { email: body.email },
+      select: {
+        id: true,
+        email: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+    return safeUser;
   }
 
   async signUp(body: AuthDto) {
     const hash = await argon.hash(body.password);
-    const user = await this.prisma.user.create({
+    const userObjc = await this.prisma.user.create({
       data: {
         email: body.email,
         password: hash,
@@ -28,6 +49,6 @@ export class AuthService {
       },
     });
 
-    return user;
+    return userObjc;
   }
 }
